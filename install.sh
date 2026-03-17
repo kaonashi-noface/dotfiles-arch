88752e11f9ceeeee08f8f994bda8598b1cbd165d
no_face <dev@paulckim.com>
2026-03-17 05:07:41 -0700

    switch from fnm & manual rustup to mise-en-place

--- install.sh
+++ install.sh
@@ -4,18 +4,15 @@
 pacman -S --needed base-devel linux-headers linux-lts-headers \
     nvidia-utils nvidia-settings mesa-utils nvidia-dkms \
     fish nvim less tree yazi zip unzip \
-    rustup fnm lua git github-cli opencode cava \
+    rustup mise lua git github-cli opencode cava \
     ttf-meslo-nerd ttf-firacode-nerd
 
 chsh -s /usr/bin/fish
 
-# Install the lasest stable version of Rust
-rustup default stable
-# Install the latest stable version of NodeJS
-touch ./config/fish/completions/fnm.fish
-fnm completions --shell fish > fnm.fish
-fnm install --lts
-fnm install --latest
+# TODO: automate copying .config
+
+# Install the latest stable versions:
+mise install
 
 # TODO: add proper pushd steps
 git clone https://aur.archlinux.org/paru.git
